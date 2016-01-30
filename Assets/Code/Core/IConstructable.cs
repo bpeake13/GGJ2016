28b83f3d955b385592f85c9dcac5ca8bafd7b110
bpeake13 <bennypeake@gmail.com>
2016-01-29 21:58:12 -0500

    Created IConstructable interface for monobehaviours

---
+++ Assets/Code/Core/IConstructable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// An object that can be constructed.
+/// </summary>
+/// <typeparam name="T">The constructor parameter type.</typeparam>
+public interface IConstructable<T>
+{
+    void Construct(T paramater);
+}
