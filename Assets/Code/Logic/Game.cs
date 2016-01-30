2c6394d9aaeaeb0d04006d180090f1b60b5c1687
bpeake13 <bennypeake@gmail.com>
2016-01-29 23:10:26 -0500

    Added game logic class and a few meta files for folders

---
+++ Assets/Code/Logic/Game.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Game : MonoBehaviour
+{
+    protected abstract void OnStartGame();
+
+    private void Start()
+    {
+        OnStartGame();
+    }
+}
